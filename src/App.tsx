a2bd16a6931da12e3cf4e31ae5da87e92fdf8822
karakulov.web.dev@gmail.com <karakulov.web.dev@gmail.com>
2023-03-31 15:04:25 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
       {
         isHuman === false ? (
           <Captcha
+            api='https://93.175.9.104:5002'
+            sitekey='10000000-ffff-ffff-ffff-000000000001'
             onSuccess={() => { setIsHuman(true) }}
           />
         ) : (
--- src/Captcha.tsx
+++ src/Captcha.tsx
@@ -1,24 +1,24 @@
 import React, { FC, useEffect, useState } from 'react'
 
-const SITEKEY = "10000000-ffff-ffff-ffff-000000000001"
-const API_URL = 'https://93.175.9.104:5002'
 const global: any = window
 
 let isLoad: boolean = false
 
 interface Props {
+  api: string
+  sitekey: string
   onSuccess: () => void
 }
 
-const Captcha: FC<Props> = ({ onSuccess }) => {
+const Captcha: FC<Props> = ({ api, sitekey, onSuccess }) => {
   const [containerId] = useState(Math.random().toString())
 
   const init = () => {
     try {
       global.Humanometr.init({
         containerId,
-        sitekey: SITEKEY,
-        api: `${API_URL}`,
+        sitekey,
+        api,
         onPassCallback: onSuccess,
         onErrorCallback: (err: any) => { console.error(new Error('onErrorCallback')) }
       })
@@ -33,7 +33,7 @@ const Captcha: FC<Props> = ({ onSuccess }) => {
     } else {
       const head = document.getElementsByTagName('head')[0]
       const script = document.createElement('script')
-      script.src = `${API_URL}/humanometr.js`
+      script.src = `${api}/humanometr.js`
       script.type = "text/javascript"
       script.onload = () => {
         isLoad = true
